da9eaae850eed7521a2a48a4d27474d655caab42
Sérgio Agostinho <sergio.agostinho@iit.it>
2019-03-01 11:53:21 +0100

    "That's how you write a Makefile right? It's just a bunch of gcc statements on a make.sh" -_-

--- experiments/scripts/demo.sh
+++ experiments/scripts/demo.sh
@@ -5,8 +5,9 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
+export LD_LIBRARY_PATH="$HOME/.miniconda3/envs/posecnn/lib/python2.7/site-packages/tensorflow"
 
-time ./tools/demo.py --gpu 0 \
+time python ./tools/demo.py --gpu 0 \
   --network vgg16_convs \
   --model data/demo_models/vgg16_fcn_color_single_frame_2d_pose_add_lov_iter_160000.ckpt \
   --imdb lov_keyframe \
